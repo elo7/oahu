98642929e13840eedef0271b5cebc592af57e88e
Miguel Bassila <developer+miguelbassila@elo7.com>
2015-12-09 17:59:15 -0200

    Initial commit

---
+++ Oahu/Oahu.h
@@ -0,0 +1,19 @@
+//
+//  Oahu.h
+//  Oahu
+//
+//  Created by Developer on 12/9/15.
+//  Copyright © 2015 Developer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Oahu.
+FOUNDATION_EXPORT double OahuVersionNumber;
+
+//! Project version string for Oahu.
+FOUNDATION_EXPORT const unsigned char OahuVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Oahu/PublicHeader.h>
+
+
