7cb4d1acdfec9675346a0e99230a1c27e735eccd
Miguel Bassila <tech+miguelbassila@elo7.com>
2015-12-21 17:25:35 -0200

    Update init constructor for ScriptMessageHandler

--- Oahu/ScriptMessageHandler.swift
+++ Oahu/ScriptMessageHandler.swift
@@ -5,7 +5,7 @@ public class ScriptMessageHandler {
     private(set) var name: String
     private(set) var handler: ((String?) -> ())
 
-    public init(name: String, handler: (String?) -> ()) {
+    public init(forEventName name: String, handler: (String?) -> ()) {
         self.name = name
         self.handler = handler
     }
